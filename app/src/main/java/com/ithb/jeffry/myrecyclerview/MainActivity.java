d55cb71795ec42e99c746cc7a6a67b319b8dc010
Jeffry Saputra <jeffrysaputra97@yahoo.com>
2018-08-13 23:09:21 +0700

    Mengimplementasikan Grid Recycler View

--- app/src/main/java/com/ithb/jeffry/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/ithb/jeffry/myrecyclerview/MainActivity.java
@@ -2,6 +2,7 @@ package com.ithb.jeffry.myrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
@@ -33,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
         rvCategory.setAdapter(listPresidentAdapter);
     }
 
+    private void showRecyclerGrid() {
+        rvCategory.setLayoutManager(new GridLayoutManager(this, 2));
+        GridPresidentAdapter gridPresidentAdapter = new GridPresidentAdapter(this);
+        gridPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(gridPresidentAdapter);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -43,9 +51,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_list:
+                showRecyclerList();
                 break;
 
             case R.id.action_grid:
+                showRecyclerGrid();
                 break;
 
             case R.id.action_cardview:
