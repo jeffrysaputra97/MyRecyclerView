be8fb167d9596adc181f79b6a190080408d34e90
Jeffry Saputra <jeffrysaputra97@yahoo.com>
2018-08-13 22:49:30 +0700

    Initial Commit
    Implementing List Recycler View

---
+++ app/src/main/java/com/ithb/jeffry/myrecyclerview/ListPresidentAdapter.java
@@ -0,0 +1,71 @@
+package com.ithb.jeffry.myrecyclerview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
+
+public class ListPresidentAdapter extends RecyclerView.Adapter<ListPresidentAdapter.CategoryViewHolder> {
+    private Context context;
+    private ArrayList<President> listPresident;
+
+    ListPresidentAdapter(Context context) {
+        this.context = context;
+    }
+
+    ArrayList<President> getListPresident() {
+        return listPresident;
+    }
+
+    void setListPresident(ArrayList<President> listPresident) {
+        this.listPresident = listPresident;
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemRow = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row_president, parent, false);
+        return new CategoryViewHolder(itemRow);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+        holder.tvName.setText(getListPresident().get(position).getName());
+        holder.tvRemarks.setText(getListPresident().get(position).getRemarks());
+
+        Glide.with(context)
+                .load(getListPresident().get(position).getPhoto())
+                .apply(new RequestOptions().override(55, 55))
+                .transition(withCrossFade())
+                .into(holder.imgPhoto);
+    }
+
+    @Override
+    public int getItemCount() {
+        return getListPresident().size();
+    }
+
+    class CategoryViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName;
+        TextView tvRemarks;
+        ImageView imgPhoto;
+
+        CategoryViewHolder(View itemView){
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tv_item_name);
+            tvRemarks = itemView.findViewById(R.id.tv_item_remarks);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+        }
+    }
+}
